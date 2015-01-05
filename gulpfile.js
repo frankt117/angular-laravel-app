e25a25daf170196e8752a0870a89456246840ec6
frankt117 <frankjtorresjr@gmail.com>
2015-01-04 20:45:17 -0600

    Added uglify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
-
-
-
+var uglify = require('gulp-uglify');
 
 
 gulp.task('annotate', function () {
   return gulp.src('public/view/**/*.js')
     .pipe(ngAnnotate())
     .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('compress', function() {
+  return gulp.src('public/view/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public/view'))
 });
