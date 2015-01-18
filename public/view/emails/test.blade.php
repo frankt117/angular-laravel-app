83efe0afb962c4cd245b42f2fc8e443bff8d6045
frankt117 <frankjtorresjr@gmail.com>
2015-01-18 14:01:34 -0600

    Quick fix.

--- public/view/emails/test.blade.php
+++ public/view/emails/test.blade.php
@@ -1,3 +1,3 @@
 <h1>GetMePro</h1>
 
-<p>THIS IS A TEST - A customer is wanting to negotiate with you. <a href='http://getmepro.com/#/app/negotiate/sp_response/{{ $msg }}'>Click here.</a></p>
+<p>A customer is wanting to negotiate with you. <a href='http://getmepro.com/#/app/negotiate/sp_response/{{ $msg }}'>Click here.</a></p>
